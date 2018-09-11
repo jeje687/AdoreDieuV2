7bc2f59ccfbd4f576adcabaa8ec246b887d24c2f
Jérémy <barbe.jeremy66@hotmail.fr>
2018-09-11 11:31:00 +0200

    initial commit]

---
+++ src/views/Thoughts/ThoughtContainer.jsx
@@ -0,0 +1,13 @@
+//@flow
+import React, { Component } from "react";
+
+interface Props {}
+interface State {}
+
+export default class ThoughtContainer extends Component<Props, State> {
+
+    render() {
+        return <div />;
+    }
+
+}
---
+++ src/views/Thoughts/ThoughtView.jsx
@@ -0,0 +1,9 @@
+//@flow
+
+import React, { Component } from "react";
+
+export default class ThoughtView extends Component<Props, State> {
+    render() {
+        return <div />;
+    }
+}
