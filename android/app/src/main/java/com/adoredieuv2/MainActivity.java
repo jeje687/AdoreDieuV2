0fb7a39dee873092f0bb8fc6b0cd23580b686d94
Jérémy <barbe.jeremy66@hotmail.fr>
2018-09-14 15:35:29 +0200

    [FEATURE splashcreen] Add Twitter like splashscreen

--- android/app/src/main/java/com/adoredieuv2/MainActivity.java
+++ android/app/src/main/java/com/adoredieuv2/MainActivity.java
@@ -1,8 +1,21 @@
 package com.adoredieuv2;
+import android.os.Bundle;
 
 import com.facebook.react.ReactActivity;
 
+import org.devio.rn.splashscreen.SplashScreen;
+
 public class MainActivity extends ReactActivity {
+
+    /**
+     * Override onCreate to implement react-native-splash-screen
+     * @param savedInstanceState
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen.show(this);
+        super.onCreate(savedInstanceState);
+    }
 
     /**
      * Returns the name of the main component registered from JavaScript.
